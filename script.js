c974611568d0653d2b06677e9680e6a91a602152
phubinh0211 <91386173+phubinh0211@users.noreply.github.com>
2022-08-12 21:00:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Bài 1
+
+function inSoThuTu() {
+  let N = 8;
+
+  for (let i = 0; i <= N; i++) {
+    console.log(i);
+  }
+}
+inSoThuTu()
+//--------------------------------------------------
+console.log("----------------------------------------------------------")
+// Bài 2
+function chiaHetCho2Va3() {
+  let a = 7;
+  if ((a % 2 === 0, a % 3 === 0)) {
+    console.log("TRUE");
+  } else {
+    console.log("FALSE");
+  }
+}
+chiaHetCho2Va3()
